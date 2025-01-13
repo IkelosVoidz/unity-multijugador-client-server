ce5f1d6efcb329ec410e1c1e1c51a40161d9161c
IkelosVoidz <114417148+IkelosVoidz@users.noreply.github.com>
2025-01-13 21:38:59 +0100

    flechas arregladas

--- server-multijugador/Assets/_Scripts/ArrowBehaviour.cs
+++ server-multijugador/Assets/_Scripts/ArrowBehaviour.cs
@@ -17,8 +17,7 @@ public class ArrowBehaviour : MonoBehaviour
         Destroy(gameObject);
     }
 
-
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         bool isEnemy = other.gameObject.layer == LayerMask.NameToLayer("Enemy");
 
